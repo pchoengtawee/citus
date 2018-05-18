57f9942996c0d3607db65f0d0b4817561326474f
Brian Cloutier <github@briancloutier.com>
2018-05-17 17:03:33 -0700

    Add simple router executor failure test

---
+++ src/test/regress/sql/failure_router_executor.sql
@@ -0,0 +1,37 @@
+SELECT citus.mitmproxy('flow.allow()');
+
+SET citus.shard_count = 2; -- one per worker
+SET citus.shard_replication_factor = 2; -- one shard per worker
+
+CREATE TABLE agg_test (id int, val int, flag bool, kind int);
+SELECT create_distributed_table('agg_test', 'id');
+INSERT INTO agg_test VALUES (1, 1, true, 99), (3, 2, false, 99), (3, 3, true, 88);
+
+-- the correct answer
+SELECT bool_or(flag) FROM agg_test WHERE id = 3;
+
+-- block queries. the other worker still exists though, so this should still work
+SELECT citus.mitmproxy('flow.contains(b"bool_or").kill()');
+SELECT bool_or(flag) FROM agg_test WHERE id = 3;
+
+SELECT citus.mitmproxy('flow.allow()');
+DROP TABLE agg_test;
+
+SET citus.shard_replication_factor = 1;  -- workers contain disjoint subsets of the data
+
+CREATE TABLE agg_test (id int, val int, flag bool, kind int);
+SELECT create_distributed_table('agg_test', 'id');
+INSERT INTO agg_test VALUES (1, 1, true, 99), (2, 2, false, 99), (2, 3, true, 88);
+
+  -- it should work
+SELECT citus.mitmproxy('recorder.reset()');
+SELECT bool_or(flag) FROM agg_test WHERE id = 2;
+SELECT count(1) FROM citus.mitmproxy('recorder.dump()'); -- a query was sent to the worker
+
+  -- the query should fail, since we can't reach all the data it wants to hit
+SELECT citus.mitmproxy('flow.contains(b"bool_or").kill()');
+SELECT bool_or(flag) FROM agg_test WHERE id = 2;
+SELECT count(1) FROM citus.mitmproxy('recorder.dump()'); -- a response was blocked
+
+SELECT citus.mitmproxy('flow.allow()');
+DROP TABLE agg_test;
